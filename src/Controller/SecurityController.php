5a5787e71bd3a58b6bb2abbc14c96734d79a5462
kenza1-1 <iderkenza1@gmail.com>
2020-04-14 01:08:22 +0000

    ajout de l'authentifcation(inscription)

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(Request $request,EntityManagerInterface $manager,UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User;
+        $form= $this->createForm(RegistrationType::class , $user);
+        $form->handleRequest($request); //analyse la requet http (les entrees dans les champs d'inscription)
+        if($form->isSubmitted() && $form->isValid()){ //si le fomrulaire est soumet et les champs son valide
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+            $manager->persist($user);
+            $manager->flush($user);
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'controller_name' => 'SecurityController',
+            'form'=>$form->createView()
+        ]);
+    }
+}
